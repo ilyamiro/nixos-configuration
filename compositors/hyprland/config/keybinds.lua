48b996fde014622e34e7072839b43485f62295b7
Goutham Krishna <224089015+gouthamkrishnap@users.noreply.github.com>
2026-09-06 02:43:59 +0530

    fix: add previous/next media key bindings

--- compositors/hyprland/config/keybinds.lua
+++ compositors/hyprland/config/keybinds.lua
@@ -37,6 +37,8 @@ hl.bind(mainMod .. " + L", hl.dsp.exec_cmd("serpantinum lock"), { repeating = tr
 hl.bind(mainMod .. " + SPACE", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
+hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
 hl.bind("XF86AudioMicMute", hl.dsp.exec_cmd("serpantinum volume mic-toggle"), { locked = true })
 hl.bind("XF86AudioMute", hl.dsp.exec_cmd("serpantinum volume mute-toggle"), { locked = true })
 hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("serpantinum volume lower"), { repeating = true, locked = true })
